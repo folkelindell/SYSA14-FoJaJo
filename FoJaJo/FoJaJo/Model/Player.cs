a41ec8efc2e290cbca9fc07e675816f3e0426ce7
Folke <fo6485ni-s@student.lu.se>
2018-02-11 17:15:47 +0100

    Player

---
+++ FoJaJo/FoJaJo/Model/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace FoJaJo.Model
+{
+    public class Player
+    {
+        [MaxLength(30)]
+        [Required]
+        [Key]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Password { get; set; }
+
+        [Required]
+        public int Rating { get; set; }
+       
+    }
+}
--- FoJaJo/FoJaJo/Program.cs
+++ FoJaJo/FoJaJo/Program.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using FoJaJo.Model;
 namespace FoJaJo
 {
     class Program
     {
         static void Main(string[] args)
         {
+            GameContext gc = new GameContext();
+            Player p = new Player();
+            p.Username = "adsd";
+            p.Email = "adsd";
+            p.Password = "adsad";
+            p.Rating = 2;
+            gc.Players.Add(p);
+            gc.SaveChanges();
         }
     }
 }
