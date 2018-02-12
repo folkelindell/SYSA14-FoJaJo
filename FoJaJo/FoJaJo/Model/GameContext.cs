3fd176b9d752f2f1a87664ebce11999f1926273a
MjauKatten <35685806+Smeeken@users.noreply.github.com>
2018-02-12 15:10:08 +0100

    added game

--- FoJaJo/FoJaJo/Model/GameContext.cs
+++ FoJaJo/FoJaJo/Model/GameContext.cs
@@ -11,6 +11,7 @@ namespace FoJaJo.Model
         public DbSet<Player> Players { get; set; }
         public DbSet<PlayerInGame> PlayersInGame { get; set; }
         public DbSet<Result> Results { get; set; }
+        public DbSet<Game> Games { get; set; }
 
 
         public GameContext() : base("LuffarSchack") { }
