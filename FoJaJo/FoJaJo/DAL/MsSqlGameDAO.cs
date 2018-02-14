63fe57cd061df2c58287b0f67f2f52eb68e7aa00
jacobtornqvist <35672110+jacobtornqvist@users.noreply.github.com>
2018-02-14 12:31:55 +0100

    Dal, Controller

--- FoJaJo/FoJaJo/DAL/MsSqlGameDAO.cs
+++ FoJaJo/FoJaJo/DAL/MsSqlGameDAO.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FoJaJo.Controller
+namespace FoJaJo.DAL
 {
-    class PasswordController
+    class MsSqlGameDAO
     {
     }
 }
