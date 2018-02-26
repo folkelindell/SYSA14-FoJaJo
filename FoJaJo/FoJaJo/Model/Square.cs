1a702c76c0300b497cad17fb575f7ded1399f64d
folkelindell <fo6485ni-s@student.lu.se>
2018-02-26 18:47:10 +0100

    varför blev de inte addade från början?

---
+++ FoJaJo/FoJaJo/Model/Square.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoJaJo.Model
+{
+    public class Square
+    {
+        public delegate void valueChangedEvent();
+        public valueChangedEvent ValueChanged { get; set; }
+        private int squareValue;
+        public int SquareValue { get
+            {
+                return squareValue;
+            }
+            set
+            {
+                squareValue = value;
+                ValueChanged?.Invoke();
+            }
+        }
+
+        public Square()
+        {
+            SquareValue = 0;
+        }
+
+    }
+}
